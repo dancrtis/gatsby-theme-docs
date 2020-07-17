dc39df265bad8bebc7079a16c2ca553c9c4b6f2c
Curtis <35540545+danielcurtis@users.noreply.github.com>
2020-07-17 12:00:07 -0400

    Initial commit

---
+++ src/styles/theme.js
@@ -0,0 +1,15 @@
+export default {
+  colors: {
+    primary: '#10C285',
+    background: '#FFFFFF',
+    headings: 'black',
+    sidebar: {
+      background: '#F5F5FA',
+      link: '#737380',
+      heading: '#aaa',
+      linkActive: '#10C285',
+      itemActive: '#FFFFFF',
+      footer: '#A8A8B3',
+    },
+  },
+};
